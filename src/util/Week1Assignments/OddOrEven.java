1a429c5fb895dceef9f200820d09ecaaf31e21b9
Rolan228123 <RolanVataev22@gmail.com>
2023-09-28 14:18:13 -0400

    OdorEven Rolan

---
+++ src/util/Week1Assignments/OddOrEven.java
@@ -0,0 +1,30 @@
+
+package Week1Assignments;
+
+public class OddOrEven {
+
+
+    public static void main(String[] args) {
+
+        Odd_Even(9);
+
+
+
+    }
+
+    public static void  Odd_Even (int number){
+
+
+        if (number % 2 == 0){
+            System.out.println("---> Even");
+        }else {
+            System.out.println("---> Odd");
+        }
+
+
+    }
+
+
+
+
+}
