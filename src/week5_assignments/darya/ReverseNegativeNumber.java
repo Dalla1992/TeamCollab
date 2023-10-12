26bb11490a5910bc6c1d99e4b1ffcfd8659ad6c0
daryamatseyuk <matdasha95@gmail.com>
2023-10-12 12:46:45 -0400

    Created reverseNegativeNumber method

---
+++ src/week5_assignments/darya/ReverseNegativeNumber.java
@@ -0,0 +1,35 @@
+package week5_assignments.darya;
+
+public class ReverseNegativeNumber {
+
+//Numbers -- Reverse negative number
+//Write a return method that can reverse negative number and return it as int
+
+    public static int reverseNegativeNumber(int number){
+
+        if (number < 0){
+
+            String numberString = String.valueOf(number); //-3567
+            String positiveNumber = numberString.substring(1); //3567
+            String reversedString = new StringBuffer(positiveNumber).reverse().toString();//7653
+            String reversedNegativeString = ("-" + reversedString);
+            System.out.println("reversedNegativeString = " + reversedNegativeString);
+            return Integer.parseInt(reversedNegativeString);
+
+        }else{
+            System.err.println("Please provide a NEGATIVE number");
+            return number;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int num = -345;
+        int revNum = reverseNegativeNumber(num);
+
+        System.out.println("num = " + num);
+        System.out.println("revNum = " + revNum);
+    }
+
+}
