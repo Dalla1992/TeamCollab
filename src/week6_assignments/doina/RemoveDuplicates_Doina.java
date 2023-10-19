ec7fd99d1021cc1bac763dd9d8f3408aa89abc97
Doinellaa <Dmacovei2014@gmail.com>
2023-10-18 21:34:37 -0400

    Task solutions for RemoveDuplicates, SameLetter, FrequencyOfChars

---
+++ src/week6_assignments/doina/RemoveDuplicates_Doina.java
@@ -0,0 +1,24 @@
+package src.week6_assignments.doina;
+
+public class RemoveDuplicates_Doina {
+
+    public static String removeDup(String str) {
+
+        // Initialize an empty string to store the result without duplicates
+        String result = "";
+
+        // Iterate through each character in the string
+        for (int i = 0; i < str.length(); i++) {
+
+            // Check if the current character is not already in the result string
+            if (!result.contains("" + str.charAt(i))) {
+
+                // If the character is not in the result, add it to the result string
+                result += "" + str.charAt(i);
+            }
+        }
+
+        // Return the modified string with duplicates removed
+        return result;
+    }
+}
