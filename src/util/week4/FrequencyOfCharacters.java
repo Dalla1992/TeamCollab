f843d0b21c355eb239f4f22eef73d8a00e41534f
Rolan228123 <RolanVataev22@gmail.com>
2023-10-19 13:42:06 -0400

    Rolan Week4 HomeWork

--- src/util/week4/FrequencyOfCharacters.java
+++ src/util/week4/FrequencyOfCharacters.java
@@ -2,6 +2,36 @@ package util.week4;
 
 public class FrequencyOfCharacters {
 
+    public static void main(String[] args) {
+
+        frequency("AAABBCDD");
+
+
+
+
+    }
+
+    public static void frequency(String str){
+        String characters = "";
+        int count;
+
+        for (int i = 0; i < str.length();i++){
+            count=0;
+
+            for (int j = 0; j < str.length();j++){
+              if (str.charAt(j) == str.charAt(i)){
+                  count++;
+              }
+              if (j == str.length()-1){
+                  if (!characters.contains("" + str.charAt(i))){
+                      characters += "" + str.charAt(i) + count;
+                  }
+              }
+            }
+        }
+        System.out.println(characters);
+
+    }
 
 
 
