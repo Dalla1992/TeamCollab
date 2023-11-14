363a2e538a1f8242c7e825b3b29b8ef5c4aef452
yaroslavLarionov <yarillo@yahoo.com>
2023-11-13 19:42:42 -0500

    added solutions for week 9 assignments

---
+++ src/week9_assignments/yaroslav/FindMinimum.java
@@ -0,0 +1,22 @@
+package week9_assignments.yaroslav;
+
+public class FindMinimum {
+    public static void main(String[] args) {
+
+        //Write a method that can find the maximum number from an int Array
+
+        System.out.println(findMax(new int[] {1, 2, 3, 4, 5}));   // 5
+        System.out.println(findMax(new int[] {5, 5, 3, 1}));      // 5
+        System.out.println(findMax(new int[] {33, 1, 2, 32}));    // 33
+    }
+
+    public static int findMax(int[] arr) {
+        int maxNum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxNum) {
+                maxNum = arr[i];
+            }
+        }
+        return maxNum;
+    }
+}
